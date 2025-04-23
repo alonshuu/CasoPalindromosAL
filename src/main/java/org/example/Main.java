c8c8a3737f1ef7b8cfabc503244f00157d53d4ac
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-23 15:59:35 -0400

    Creación del readme, y del paso 2 de la actividad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
     public static void main(String[] args){
-        System.out.println(esPalindromo("anita lava la tina"));
+        System.out.println(esPalindromo("aca"));
     }
 
 
