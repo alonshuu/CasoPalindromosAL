239d0717ee6af7d1ce3a607f4e0be252aab046d3
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-23 15:53:28 -0400

    Creación del readme, y del paso 2 de la actividad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,29 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    public static void main(String[] args){
+        System.out.println(esPalindromo("anita lava la tina"));
+    }
+
+
+
+
+    public static boolean esPalindromo(String palabra){
+
+        char[] caracteres = palabra.toCharArray();
+        char[] invertido = new char[caracteres.length];
+        for (int i = 0; i < caracteres.length; i++){
+            invertido[i] = caracteres[caracteres.length - 1 - i];
+        }
+
+        String resultado = new String(invertido);
+
+        if (resultado.equals(palabra)){
+            return true;
+        } else {
+            return false;
         }
     }
 }
